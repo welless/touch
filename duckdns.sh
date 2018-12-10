c16692c5430bc1e2cc72d9d15734645182720d11
well <pw04_5@163.com>
2018-12-10 13:59:18 +0800

    Update duckdns.sh

--- duckdns.sh
+++ duckdns.sh
@@ -9,14 +9,14 @@ token=xxxx
 
 curl -s http://icanhazip.com > /tmp/ip.txt
 myip=$(head -1 /tmp/ip.txt)
-echo "current IP:"$myip
+#echo "current IP:"$myip
 
 current=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ "$mydnsip" = "$myip" ] ; then
     echo $current $myip 'DDNS IP is not change'
 else
-    http_code=$(curl -s -S -w "%{http_code}" "https://www.duckdns.org/update?domains=npe&token=$token&ip=$myip")
+    http_code=$(curl -s -w "%{http_code}" "https://www.duckdns.org/update?domains=npe&token=$token&ip=$myip")
     if [ $http_code = 'OK200' ] ; then
         echo $current $mydnsip ' => ' $myip 'update DDNS success'
         echo $myip > /tmp/dnsip.txt
